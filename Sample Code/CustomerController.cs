7e54b05e9656bed17523824c75695de8a8cfaa97
Lilantha Ratnapala <lilantha@openqubit.com>
2017-06-19 11:09:03 +0530

    Add files via upload

---
+++ Sample Code/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Threading.Tasks;
+using IJSE.PosClient.Presentation.Web.WebApi;
+using IJSE.PosClient.Presentation.Web.Models;
+
+namespace IJSE.PosClient.Presentation.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //public async Task<ActionResult> Index()
+        //{
+        //    WebClient client = new WebClient();
+        //    Customer customer = await client.GetCustomer();
+
+        //    return View(customer);
+        //}
+
+        public async Task<ActionResult> Index()
+        {
+            WebClient client = new WebClient();
+            Customer customer = new Customer { ID = 45, Name = "ClientCustomer", Address = "No25", Creditlimit = 25000, Tel = "0112" };
+
+            await client.AddCustomer(customer);
+
+            return View(customer);
+        }
+
+
+    }
+}
---
+++ Sample Code/WebClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Net.Http;
+using IJSE.PosClient.Presentation.Web.Models;
+using System.Web.Script.Serialization;
+using System.Threading.Tasks;
+
+namespace IJSE.PosClient.Presentation.Web.WebApi
+{
+    public class WebClient
+    {
+        HttpClient _client;
+        JavaScriptSerializer _jsonSerializer = new JavaScriptSerializer();
+
+        public WebClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<Customer> GetCustomer()
+        {
+            Customer customer = new Customer();
+
+            using (var response = await _client.GetAsync("http://localhost:51279/api/Customer/1"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    customer = _jsonSerializer.Deserialize<Customer>(jsonString);
+                }
+            }
+
+
+            return customer;
+
+        }
+
+
+        public async Task<bool> AddCustomer(Customer newCustomer)
+        {
+            
+
+            var webServiceData = new MultipartFormDataContent();
+
+            webServiceData.Add(new StringContent(_jsonSerializer.Serialize(newCustomer)), "newCustomer");
+
+
+            // string json = _jsonSerializer.Serialize(customer);
+
+            using (var response = await _client.PostAsync("http://localhost:51279/api/AddCustomer/", webServiceData)) 
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                   // customer = _jsonSerializer.Deserialize<Customer>(jsonString);
+                }
+            }
+
+
+            return true;
+
+        }
+
+    }
+}
